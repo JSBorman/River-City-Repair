135403b43987ec98a0e51942f52ff863d0017a4d
Dan Gorman <danthemangorman@gmail.com>
2016-11-27 17:47:54 -0500

    fixed gitignore, broke things, fixed them

---
+++ README.md
@@ -0,0 +1,3 @@
+# GameDev-Project-4
+
+Prompt: A low-income family in Flint, Michigan
---
+++ UI & Stat Scene/Assets/Scripts/StatObj.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+/* StatObj is the class used to control & update each player stat
+ *
+ * To Do:
+ * 	- Should players be allowed to make themselves lose by purchasing enough negative items? 
+*/
+
+public class StatObj : MonoBehaviour {
+
+	public string stat_name;
+	public int default_value;
+	public int max_value;
+	int curr_value;
+
+	Text text;
+	
+	void Start () {
+		curr_value = default_value;
+		text = GetComponent<Text> ();
+	}
+	
+	// Prints "		Name: value"
+	void Update () {
+		text.text = "\t" + stat_name + ": " + curr_value;
+	}
+
+	//Change the value of this stat
+	public void addValue(int delta){
+		if (curr_value + delta >= getMaxValue ())	//Don't overload
+			curr_value = max_value;
+	//	else if (curr_value + delta <= 0) {			//Needs to be able to be negative for groceries
+	//		curr_value = 0;
+	//	}
+		else
+			curr_value += delta;
+	}
+
+	public string getName(){
+		return stat_name;
+	}
+
+	public int getValue(){
+		return curr_value;
+	}
+
+	public int getMaxValue(){
+		return max_value;
+	}
+}
