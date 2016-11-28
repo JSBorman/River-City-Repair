20e313c7b6e567f85c376770f828577a78fba257
Dan Gorman <danthemangorman@gmail.com>
2016-11-27 19:49:29 -0500

    renamed unity project for computer usability

---
+++ HardDaysNight/Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    //Game manager static instance, use this for persistent management
+    public static GameManager Instance;
+
+	// Use this for initialization
+	void Start () {
+        if (GameManager.Instance = null)
+        {
+            GameManager.Instance = this;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
