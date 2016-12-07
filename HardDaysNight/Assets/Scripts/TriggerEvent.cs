bbfb266e703f9ae770b4e4fd0aae647ac2f4a1b5
Staethe <jzmynb@gmail.com>
2016-12-07 00:23:04 -0500

    Added for transitioning out of random events

---
+++ HardDaysNight/Assets/Scripts/TriggerEvent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* For use on selection of random event choice */
+public class TriggerEvent : MonoBehaviour {
+
+	public int fam;
+	public int hp;
+	public int happy;
+
+	public void onClickMe(){
+		GameManager.Instance.updatePlayerFam (fam);
+		GameManager.Instance.updatePlayerHP (hp);
+		GameManager.Instance.updatePlayerHappy (happy);
+
+		//Trigger Next scene
+		GameManager.Instance.nextLevel ();
+	}
+}
